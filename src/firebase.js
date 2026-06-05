7446f628a0a665b8e72cc13df6a40d8e0847c25f
greenland wangjam <greenland.wangjam@gmail.com>
2026-06-05 19:48:15 +0530

    Security issue fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,15 @@
 import { initializeApp } from "firebase/app";
-
-import { getFirestore }
-from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM7ZswouIl1v4y94VqguKRdDKSX9uMb5s",
-  authDomain: "learniq-6455f.firebaseapp.com",
-  projectId: "learniq-6455f",
-  storageBucket: "learniq-6455f.firebasestorage.app",
-  messagingSenderId: "334650651193",
-  appId: "1:334650651193:web:0df256df91697e2fae53e8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app =
-  initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const db =
-  getFirestore(app);
+export const db = getFirestore(app);
